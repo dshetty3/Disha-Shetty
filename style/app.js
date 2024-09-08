57d8217abeca2003ec73cd2b32e92ad5494e390d
Disha Shetty <dshetty3@binghamton.edu>
2024-09-07 21:26:33 -0400

    Changes in Into

--- style/app.js
+++ style/app.js
@@ -42,4 +42,52 @@ function isItemInView(item){
             imageElement.src = 'img/logo.jpeg';
         }
     });
+
+
+    // Splash Screen
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo-header');
+let logoSpan = document.querySelectorAll('.logo');
+
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    setTimeout(()=>{
+
+        logoSpan.forEach((span, idx)=>{
+            setTimeout(()=>{
+                span.classList.add('active');
+            }, (idx + 1) * 400)
+        });
+        setTimeout(()=>{
+            logoSpan.forEach((span, idx)=>{
+                setTimeout(()=>{
+                    span.classList.remove('active');
+                    span.classList.add('fade');
+                },(idx + 1) * 50)
+            })
+        }, 2000)
+
+        setTimeout(()=>{
+            intro.style.top = '-100vh';
+        }, 2300)
+    })
+
+    //Hello Change
+    const languages = ["Hello.", "مرحبا.", "नमस्ते.", "你好.", "Holá."];
+
+        let currentIndex = 0;
+
+        function rotateText() {
+            const rotatingTextElement = document.getElementById("rotating-text");
+            rotatingTextElement.textContent = languages[currentIndex];
+            currentIndex = (currentIndex + 1) % languages.length;
+        }
+
+        setInterval(rotateText, 400);
+
+        
+})
+
+
+
    
